7ff2573a7d3db0ec3bb10e7bac789886a1c68107
pie <returnpie@icloud.com>
2020-08-27 01:25:20 +0900

    what is body-parser

--- section/app.js
+++ section/app.js
@@ -1,18 +1,27 @@
 const express = require("express");
 
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded());
 
 app.use("/", (req, res, next) => {
   console.log("This always runs!");
+  next();
 });
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In the another middleware!");
-  res.send("<h1>The Add Product</h1>");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In the another middleware!");
   res.send("<h1>Hello from Express!</h1>");
 });
 
